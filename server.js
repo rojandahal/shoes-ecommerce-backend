37d9000df3baf94cf111ce0499706f71792b2ce3
rojandahal <maxpanye60@gmail.com>
2022-07-01 09:44:50 +0545

    credential true

--- server.js
+++ server.js
@@ -27,6 +27,7 @@ app.use((req,res,next)=>{
     res.setHeader('Access-Control-Allow-Origin',['http://localhost:3000']);
     res.setHeader('Access-Control-Allow-Methods','GET,POST,PUT,PATCH,DELETE');
     res.setHeader('Access-Control-Allow-Methods','Content-Type Authorization Origin X-Requested-With Accept');
+    res.setHeader('Access-Control-Allow-Credentials', 'true')
     next(); 
 })
 
