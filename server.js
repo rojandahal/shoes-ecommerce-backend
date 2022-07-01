7d4bdaf394ca5ad300a107b1947cd85c647488d2
rojandahal <maxpanye60@gmail.com>
2022-07-01 09:48:36 +0545

    cors install

--- server.js
+++ server.js
@@ -23,14 +23,21 @@ const shoesRoutes = require('./src/routes/shoe');
 const userRoutes = require('./src/routes/users');
 const authRoutes = require('./src/routes/auth');
 
-app.use((req,res,next)=>{
-    res.setHeader('Access-Control-Allow-Origin',['http://localhost:3000']);
-    res.setHeader('Access-Control-Allow-Methods','GET,POST,PUT,PATCH,DELETE');
-    res.setHeader('Access-Control-Allow-Methods','Content-Type Authorization Origin X-Requested-With Accept');
-    res.setHeader('Access-Control-Allow-Credentials', 'true')
-    next(); 
-})
+// app.use((req,res,next)=>{
+//     res.setHeader('Access-Control-Allow-Origin',['http://localhost:3000']);
+//     res.setHeader('Access-Control-Allow-Methods','GET,POST,PUT,PATCH,DELETE');
+//     res.setHeader('Access-Control-Allow-Methods','Content-Type Authorization Origin X-Requested-With Accept');
+//     res.setHeader('Access-Control-Allow-Credentials', 'true')
+//     next(); 
+// })
 
+const cors = require('cors');
+const corsOptions ={
+    origin:'http://localhost:3000', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
 // Body parser
 app.use(express.json());
 
